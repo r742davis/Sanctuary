c30ee66b77b91fd62e05dcc2a7a42a1986fa8a82
r742davis <richard.trow.davis@gmail.com>
2024-05-16 19:25:25 -0400

    Search functionality added + dummy code for testing

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -1,8 +1,57 @@
-import { PropsWithChildren } from "react";
+"use client";
+
+import { PropsWithChildren, useState } from "react";
 import styles from "./SearchBar.module.css";
+import classNames from "classnames";
 
-type SearchBarProps = {} & PropsWithChildren;
+type SearchBarProps = {
+  placeholder?: string;
+  handleSubmit: (keyword: string) => void;
+} & PropsWithChildren;
 
-export default function SearchBar({ children }: SearchBarProps) {
-  return <div className={styles.search}>{children}</div>;
+export default function SearchBar({
+  children,
+  handleSubmit,
+  placeholder = "Search",
+}: SearchBarProps) {
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") return handleSubmit(searchInput);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <input
+        className={styles.input}
+        value={searchInput}
+        placeholder={placeholder}
+        onChange={(e) => handleInputChange(e)}
+        onKeyDown={(e) => handleKeyDown(e)}
+      />
+      <button
+        className={classNames(styles.button, {
+          [styles["is-visible"]]: !!searchInput,
+        })}
+        onClick={(e) => handleSubmit(searchInput)}
+        title="Submit"
+      >
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M10.5858 6.34317L12 4.92896L19.0711 12L12 19.0711L10.5858 17.6569L16.2427 12L10.5858 6.34317Z"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,51 @@
+"use client";
+
 import { Card, SearchBar } from "@components";
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
-  const cardsArray = new Array(10).fill({
+  const [keyword, setKeyword] = useState("");
+
+  const cardsArray1 = new Array(3).fill({
+    title: "Oh my!",
+    content: "Singing in the rain",
+  });
+
+  const cardsArray2 = new Array(3).fill({
     title: "Hello",
     content: "Content goes here boyyy",
   });
+
+  const cardsArray3 = new Array(4).fill({
+    title: "Metals",
+    content: "Quartz is a mineral",
+  });
+
+  const combinedArrays: Array<{ title: string; content: string }> = [...cardsArray1, ...cardsArray2, ...cardsArray3];
+
+  const filterItems = (query: string) => {
+    const regex = new RegExp(query, "i");
+    return combinedArrays.filter((item) => regex.test(item.content));
+  };
+
+  const handleSubmitKeyword = (submission: string) => {
+    console.log('submit');
+    setKeyword(submission);
+  };
+
+  const filteredCards = !!keyword ? filterItems(keyword) : combinedArrays;
 
   return (
     <main className={styles.main}>
       <div className={styles.header}>
         <h1>Sanctuary</h1>
-        <SearchBar />
+        <SearchBar handleSubmit={handleSubmitKeyword} />
       </div>
       <div className={styles.content}>
         <h2>Notes</h2>
         <div className={styles.list}>
-          {cardsArray.map((card, i) => (
+          {filteredCards.map((card, i) => (
             <Card key={`card-${i}`} title={card.title} content={card.content} />
           ))}
         </div>
